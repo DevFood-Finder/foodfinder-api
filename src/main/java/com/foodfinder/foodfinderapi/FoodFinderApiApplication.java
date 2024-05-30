8a1201e5aea9a8b0b96c8c68d30b5177028f6184
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2024-05-30 00:12:32 -0500

    docs: project added

---
+++ src/main/java/com/foodfinder/foodfinderapi/FoodFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodfinder.foodfinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodFinderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodfinder/foodfinderapi/FoodFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodfinder.foodfinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFinderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
